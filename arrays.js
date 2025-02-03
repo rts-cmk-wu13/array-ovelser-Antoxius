e2487047400aa51ff7086b4f502c27190b89051d
Xenius Tolderlund <Xeniusawbt@gmail.com>
2025-02-03 10:35:06 +0100

    Opgave 4 løst med array metoden .find

--- arrays.js
+++ arrays.js
@@ -1,9 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
-    let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
-    let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
+document.addEventListener("DOMContentLoaded", function () {
+  let catsArray = [
+    "Ollie",
+    "Sophie",
+    "Salem",
+    "Tiger",
+    "Binx",
+    "Pumpkin",
+    "Penny",
+    "Lenny",
+  ];
+  let dogsArray = [
+    "Barney",
+    "Molly",
+    "Baxter",
+    "Polly",
+    "Buddy",
+    "Suki",
+    "Watson",
+  ];
+  let fruitsArray = [
+    "Banana",
+    "Orange",
+    "Pineapple",
+    "Mango",
+    "Plum",
+    "Apricot",
+  ];
+  let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
 
-    // --- Løs opgaverne herunder --- 
+  // --- Løs opgaverne herunder: Find element i catsArray'et med .find metoden ---
+//   let hasBegheera = catsArray.find(function (cat) {
+//     return cat === "Begheera";
+//   });
+//   console.log(hasBegheera);
 
-}) // ends DOMContentLoaded
+  let hasSalem = catsArray.find(function (cat) {
+    return cat === "Salem";
+  });
+  console.log(hasSalem);
+}); // ends DOMContentLoaded
